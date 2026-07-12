417a0259a2f8b2e58cc69407f3c330c77bb4b615
Wishwa Wijayananda <jananawishwa69@gmail.com>
2026-07-12 16:28:27 +0530

    add LogoSection

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Hero from "./sections/Hero"
 import NavBar from "./components/NavBar"
 import ShowCaseSection from "./sections/ShowCaseSection"
+import LogoSection from "./components/LogoSection"
 const App = () => {
   return (
     <>
       <NavBar/>
       <Hero/>
       <ShowCaseSection/>
+      <LogoSection/>
     </>
   )
 }
