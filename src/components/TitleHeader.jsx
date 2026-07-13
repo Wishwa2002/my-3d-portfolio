1528736a894d50cc6cb0d545ee013758d897c8ed
Wishwa <jananawishwa69@gmail.com>
2026-07-13 23:51:41 +0530

    add Experience Section

--- src/components/TitleHeader.jsx
+++ src/components/TitleHeader.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import {expCards} from "../constants/index.js"
 
 const TitleHeader = ({title,sub}) => {
   return (
     <div className='flex flex-col items-center gap-5'>
         <div className='hero-badge'>
             <p>{sub}</p>
+        </div>
+        <div className='font-semibold md:text-3xl text-center'>
+            {title}
+
+        </div>
+        <div className='mt-32 relative'>
+            <div className='relative z-50 xl:space-y-32 space y-10'>
+                {expCards.map((card) =>(
+                    <div key={card.title} className='exp-card-wrapper'></div>
+                ))}
+            </div>
+
         </div>
     </div>
   )
